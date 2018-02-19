23c53142c5a11fb2ea68b8fda714ab2fa67b926b
up834069 <36628804+up834069@users.noreply.github.com>
2018-02-19 13:13:06 +0000

    Add files via upload

---
+++ JobPortal/src/JobPortal/Database code.sql
@@ -0,0 +1,17 @@
+#Author UP834069#
+CREATE DATABASE JobPortal;
+
+CREATE TABLE JobSearch (
+JobType varchar (250) not null,
+Location varchar (50) not null
+);
+#Made up data to run queries#
+
+INSERT INTO JobSearch VALUES ('Electrician', 'Russia');
+INSERT INTO JobSearch VALUES ('Primary School Teacher', 'Ireland');
+INSERT INTO JobSearch VALUES ('FireFighter', 'USA');
+INSERT INTO JobSearch VALUES ('Nurse', 'Germany');
+INSERT INTO JobSearch VALUES ('Doctor', 'Japan');
+INSERT INTO JobSearch VALUES ('Zoo Keeper', 'France');
+INSERT INTO JobSearch VALUES ('Nurse', 'Spain');
+INSERT INTO JobSearch VALUES ('Lawyer', 'Belgium');
